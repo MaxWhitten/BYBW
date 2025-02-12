f86fabf2ab4c444e83823b2ad196d0e4bdc22b3c
Max <85583668+MaxWhitten@users.noreply.github.com>
2025-02-12 11:12:46 -0500

    added /public and main content

--- app.js
+++ app.js
@@ -1,26 +1,16 @@
-// Example using Express.js
-const express = require('express');
+const express = require('express'),
+    cors = require('cors'),
+    path = require('path');
+
 const app = express();
-const cors = require('cors');
-const path = require('path');
 
 app.use(cors());
 
-const rootRouter = require('./routes/root');
-const mediaRouter = require('./media/dn');
-const jsRouter = require('./js/dn');
-const cssRouter = require('./css/dn');
-const contBlogRouter = require('./content/blog/blog');
-const contProjectsRouter = require('./content/projects/projects');
-// const projectsRouter = require('./routes/projects')
+app.use('/', require('./routes/root'));
 
-app.use('/', rootRouter);
-app.use('/media', mediaRouter);
-app.use('/js', jsRouter);
-app.use('/css', cssRouter);
-app.use('/content/blog', contBlogRouter);
-app.use('/content/projects', contProjectsRouter);
-// app.use('/projects', projectsRouter);
+app.get('/media/*', (req, res) => { res.sendFile(path.join(__dirname, '/public/media/', req.params[0])) });
+app.get('/js/*', (req, res) => { res.sendFile(path.join(__dirname, '/public/js/', req.params[0])) });
+app.get('/css/*', (req, res) => { res.sendFile(path.join(__dirname, '/public/css/', req.params[0])) });
 
 // specifying server port
 const port = process.env.PORT || 3000;
